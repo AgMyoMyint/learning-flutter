e95fedeec9304cde3279b8d49149f77fdd0291a6
Aung Myo Myint <aungmyo@justlogin.com>
2022-01-29 23:25:05 +0630

    Update main.dart

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -10,12 +10,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+
+    var questions = [ "Question1 ", "QUestion 2", "Question 3"];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text ('My First App'),
           ), 
-          body: Text('This is my default text.'),
+          body: 
+          
+          Column(children : 
+          [
+            Text("Questions"),
+            RaisedButton(child: Text("Answer 1") ,onPressed: null),
+            RaisedButton(child: Text("Answer 2") ,onPressed: null),
+            RaisedButton(child: Text("Answer 3") ,onPressed: null),
+          ],
+          )
+          
+          ,
       ),
     );
   }
